ce5b291d0fd0bf7dae7110348c6d1721ecd26b91
Pedro Rodrigues <pedro.rodrigues.wch@gmail.com>
2019-12-12 11:58:12 -0300

    Removed test file

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-use neto737\AddressValidator\Validation;
-
-$validator = Validation::make('LTC');
-var_dump($validator->validate('ltc1q6xagsqvhu2c95a8lqvf09u73r5zwxqsk8nmz6j'));
